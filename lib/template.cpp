564db96a221158c7fcc9b82b7a3f0ccddf6e373b
monman53 <tetsuro53@gmail.com>
2016-07-10 04:47:34 +0900

    create lib/template.cpp
    cppのひな形をつくった。(参考: iwiさん)

---
+++ lib/template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <functional>
+#include <numeric>
+#include <limits>
+using namespace std;
+ 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define pb(e) push_back(e)
+#define mp(a, b) make_pair(a, b)
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(){
+    return 0;
+}
