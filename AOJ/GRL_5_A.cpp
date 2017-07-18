fd405471313ccdd39bd79aa43c8bd56354ef719f
monman53 <monman.cs@gmail.com>
2017-07-18 13:26:18 +0900

    [update] lib

---
+++ AOJ/GRL_5_A.cpp
@@ -0,0 +1,27 @@
+// header {{{
+// #define NDEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+// {U}{INT,LONG,LLONG}_{MAX,MIN}
+#define ALPHABET    (26)
+#define INF         INT_MAX
+#define MOD         (1000000007LL)
+#define EPS         (1e-10)
+// #define EQ(a, b)    (abs((a)-(b)) < EPS)
+inline bool EQ(double a, double b) {return abs(a-b) < EPS;}
+// ciling(x/y) = (x+y-1)/y
+
+template<class T>
+using PIT = pair<int, T>;
+template<class T>
+using PTI = pair<T, int>;
+using PII = pair<int, int>;
+using PDI = pair<double, int>;
+using LL  = long long;
+using ULL = unsigned long long;
+// }}}
+
+int main() {
+    return 0;
+}
